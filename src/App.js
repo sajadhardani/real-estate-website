2c54d923bfc9f93c4de3853d00be36ad8886b049
sajad <sajad.hardani188@gmail.com>
2025-07-17 01:59:44 +0330

    starting files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+    <h1 className='bg-blue-400 text-2xl text-white'>hi</h1>
+    </div>
+  );
+}
+
+export default App;
