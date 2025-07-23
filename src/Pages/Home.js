8653c57112f7c2f20d42060e374769ad1c6481f4
sajad <sajad.hardani188@gmail.com>
2025-07-23 21:44:26 +0330

    i call the API for showing result in search now i need to finish list so it can show the client

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Banner from '../components/Banner'
+import { House, HouseList } from '../components/House'
 
 function Home() {
   return (
     <div className='bg-blue-400'>
        <Banner />
+       <HouseList />
     </div>
   )
 }
---
+++ src/components/House.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { BiBed, BiBath, BiArea } from "react-icons/bi";
+export const House = ({ house }) => {
+  return (
+    <div
+      className="bg-white shadow-1 p-5 rounded-lg mt-16 mx-auto cursor-pointer
+    hover:shadow-2xl transition"
+    >
+      <img className="mb-8" src={house.image} />
+      <div className="bg-gray-700 rounded-full text-white px-3 py-1 inline-block">
+        {house.type}
+      </div>
+      <div className="bg-gray-700 rounded-full text-white px-3 py-1 inline-block">
+        {house.type}
+      </div>
+      <div className="text-lg font-semibold max-w-[260px] ">
+        {" "}
+        {house.address}{" "}
+      </div>
+      <div className="flex gap-x-4 my-4"></div>
+      <div className="flex items-center text-gray-600 gap-1">
+        <div className="text-[200px] rounded-full">
+          <BiBed />
+        </div>
+        <div className="text-base">{house.bedrooms}</div>
+      </div>
+      <div className="flex items-center text-gray-600 gap-1">
+        <div className="text-[200px] rounded-full">
+          <BiBath />
+        </div>
+        <div className="text-base">{house.bathrooms}</div>
+      </div>
+      <div className="flex items-center text-gray-600 gap-1">
+        <div className="text-[200px] rounded-full">
+          <BiArea />
+        </div>
+        <div className="text-base">{house.surface}</div>
+      </div>
+      <div className="text-lg font-semibold text-red-500 mb-4">
+        {house.price} تومان
+
+      </div>
+
+    </div>
+  );
+};
